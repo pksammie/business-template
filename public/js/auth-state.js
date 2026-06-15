61994b182c9a367cef9e6f80f5c2d29067a91412
Sammie <samsonpamilerin959@gmail.com>
2026-06-15 14:18:48 +0100

    codes

--- public/js/auth-state.js
+++ public/js/auth-state.js
@@ -50,7 +50,10 @@ function showUserDropdown(user, isAdmin) {
                     <div class="theme-box" data-theme="luxury"></div>
                 </div>
 
-                <div><a href="/orders" class="dropdown-link"</a><i class="fa-solid fa-box"></i> My Orders</div>
+                <div><a href="/orders" class="dropdown-link">
+    <i class="fa-solid fa-box"></i><div>
+    My Orders</div>
+</a></div>
 
                 <div class="dropdown-item danger" id="logoutBtn">Logout</div>
             </div>
